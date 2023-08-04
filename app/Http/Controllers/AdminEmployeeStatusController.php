9c6ac043fc6e2682e4a696a68128e7e7cc06073e
Kodera <kodera@timecapsuleorg.inc>
2023-08-04 17:28:13 +0900

    feat:社員ステータス一覧ページの作成

--- app/Http/Controllers/AdminEmployeeStatusController.php
+++ app/Http/Controllers/AdminEmployeeStatusController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminEmployeeStatusController extends Controller
 {
     public function index()
     {
+        $employee_status = DB::table('employee_status')
+            ->where('del_flg', false)
+            ->get()
+            ->all();
 
+        return view('admin.employee-status.index', [
+            'employee_status' => $employee_status
+        ]);
     }
 }
