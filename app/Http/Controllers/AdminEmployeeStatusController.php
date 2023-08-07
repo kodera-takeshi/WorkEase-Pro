672261ac46c5351b3bc8ceaf7e7b81ecc4de02dd
Kodera <kodera@timecapsuleorg.inc>
2023-08-07 17:01:24 +0900

    fix:PHPdocの追加

--- app/Http/Controllers/AdminEmployeeStatusController.php
+++ app/Http/Controllers/AdminEmployeeStatusController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
@@ -26,6 +27,10 @@ class AdminEmployeeStatusController extends Controller
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function update(Request $request)
     {
         $param = [
