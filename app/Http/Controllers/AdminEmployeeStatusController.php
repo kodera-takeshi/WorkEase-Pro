3e02a91391033b915c95e99f9fbe2e9e07faa4f3
Kodera <kodera@timecapsuleorg.inc>
2023-08-07 17:00:08 +0900

    feat:社員ステータス更新モーダルの実装

--- app/Http/Controllers/AdminEmployeeStatusController.php
+++ app/Http/Controllers/AdminEmployeeStatusController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 
 class AdminEmployeeStatusController extends Controller
 {
+    /**
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
     public function index()
     {
         $employee_status = DB::table('employee_status')
@@ -17,5 +24,20 @@ class AdminEmployeeStatusController extends Controller
         return view('admin.employee-status.index', [
             'employee_status' => $employee_status
         ]);
+    }
+
+    public function update(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+
+        DB::table('employee_status')
+            ->where('id', $request->id)
+            ->update($param);
+
+        // todo:更新処理ステータスのメッセージを追加
+        return Redirect::route('admin.employee-status');
     }
 }
