652e3d2904a5b4a2ca4f691b18f5d1220e9cc2a3
Kodera <kodera@timecapsuleorg.inc>
2023-08-31 17:50:00 +0900

    layout:管理者権限の更新モーダルの修正

--- resources/views/admin/components/admin_list_update_form.blade.php
+++ resources/views/admin/components/admin_list_update_form.blade.php
@@ -17,8 +17,8 @@
                 >
                 <label for="name-{{ $id }}" class="block text-sm font-semibold leading-6 text-gray-900 mt-2">役職</label>
                 <select
-                    name="name"
-                    id="name-{{ $id }}"
+                    name="role_id"
+                    id="role_id-{{ $id }}"
                     class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                 >
                     @foreach($admin_roles as $admin_role)
